2ba00a00e8d2f890bc7bfdf0d995224a98b86aae
tranduytung <tranduytung1994@gmail.com>
2017-05-01 19:51:44 +0700

    init project && bao chong trom tu xa

---
+++ app/src/main/java/com/example/duytung/x_prototype/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.duytung.x_prototype;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button anti_theft_off_btn;
+    Button anti_theft_on_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        anti_theft_off_btn = (Button)findViewById(R.id.anti_theft_off);
+        anti_theft_on_btn = (Button)findViewById(R.id.anti_theft_on);
+        anti_theft_off_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Bật báo chống trộm")
+                    .setMessage("Bạn có muốn bật chức năng báo chống trộm từ xa?")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        //                    Button chong_trom_off = (Button)findViewById(R.id.chong_trom_off);
+                        public void onClick(DialogInterface dialog, int which) {
+                            // ẩn màn hình
+                            v.setVisibility(View.GONE);
+                            anti_theft_on_btn.setVisibility(View.VISIBLE);
+                            // chuyển màn hình
+//                            Intent inten = new Intent(MainActivity.this, Main3Activity.class);
+//                            startActivity(inten);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            }
+        });
+        anti_theft_on_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Tắt báo chống trộm")
+                    .setMessage("Bạn có muốn t chức năng báo chống trộm từ xa?")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        //                    Button chong_trom_off = (Button)findViewById(R.id.chong_trom_off);
+                        public void onClick(DialogInterface dialog, int which) {
+                            // ẩn màn hình
+                            v.setVisibility(View.GONE);
+                            anti_theft_off_btn.setVisibility(View.VISIBLE);
+                            // chuyển màn hình
+//                            Intent inten = new Intent(MainActivity.this, Main3Activity.class);
+//                            startActivity(inten);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
